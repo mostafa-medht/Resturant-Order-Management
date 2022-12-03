a9acb07a31def4d197fe15070219cda86044f3b0
Mostafa Med7t <mostafamedht98@gmail.com>
2022-12-03 04:14:02 +0200

    add ingredient services test cases

--- config/database.php
+++ config/database.php
@@ -35,12 +35,18 @@ return [
 
     'connections' => [
 
+//        'sqlite' => [
+//            'driver' => 'sqlite',
+//            'url' => env('DATABASE_URL'),
+//            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+//            'prefix' => '',
+//            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+//        ],
+
         'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'driver'   => 'sqlite',
+            'database' => storage_path('database.sqlite'),
+            'prefix'   => '',
         ],
 
         'mysql' => [
