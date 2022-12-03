bd981ee51093c1370d6c270d6598ff47d559cff1
Mostafa Med7t <mostafamedht98@gmail.com>
2022-12-03 03:58:17 +0200

    add routes to api.php files

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,30 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['prefix' => 'v1'], function (){
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Ingredients routes
+    Route::group(['prefix' => 'ingredients'], function (){
+        Route::resource('', IngredientController::class)->only(['index', 'store']);
+        Route::post('/charge-ingredients', [IngredientController::class, 'chargeIngredient']);
+        Route::get('/get-ingredient', [IngredientController::class, 'show']);
+    });
+
+    // Product routes
+    Route::group(['prefix' => 'products'], function (){
+        Route::resource('', ProductController::class)->only(['index', 'store']);
+         Route::get('/get-product', [ProductController::class, 'show']);
+    });
+
+    // Ingredients routes
+    Route::group(['prefix' => 'orders'], function (){
+        Route::resource('', OrderController::class)->only(['index', 'store']);
+        Route::get('/get-order', [OrderController::class, 'show']);
+    });
+
 });
