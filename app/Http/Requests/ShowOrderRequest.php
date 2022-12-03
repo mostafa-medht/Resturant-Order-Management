28784c04947b4a8e90d379355feeb23d9c12a06a
Mostafa Med7t <mostafamedht98@gmail.com>
2022-12-03 04:09:08 +0200

    add orders requests validation

---
+++ app/Http/Requests/ShowOrderRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ShowOrderRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|numeric|exists:orders,id'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        if ($validator->fails()) {
+            throw new HttpResponseException(returnCustomValidationError('please check your data', $validator->errors()));
+        }
+    }
+}
